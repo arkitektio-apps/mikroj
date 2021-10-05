16516f3326e5a65e7bf762cd8702c1b02e3691dc
jhnnsrs <jhnnsrs@gmail.com>
2021-10-05 18:14:38 +0200

    added spec and test

--- mikroj/helper.py
+++ mikroj/helper.py
@@ -1,12 +1,23 @@
 from mikro.schema import Representation
 from mikroj.registries.helper import BaseImageJHelper, set_running_helper
 import dask
+import xarray as xr
 
 class ImageJHelper(BaseImageJHelper):
 
     def __init__(self, headless=False, bind=True, version="sc.fiji:fiji:2.1.0", plugins = []) -> None:
         if bind: set_running_helper(self)
         super().__init__(headless=headless, version=version, plugins= plugins)
+
+
+    def show_xarray(self, xarray: xr.DataArray):
+        if dask.is_dask_collection(xarray.data):
+            jimage = self.py.to_java(xarray.compute())
+        else:
+            jimage = self.py.to_java(xarray)
+        
+        self.ui.show(xarray.name, jimage)
+
 
 
     def displayRep(self, rep: Representation):
--- test.py
+++ test.py
@@ -1,16 +1,35 @@
 import sys
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
 import imagej
+import os
+import logging
+logging.basicConfig(level=logging.DEBUG)
+import scyjava
+import numpy as np
+from mikroj.helper import ImageJHelper
+import xarray as xr
+
+#os.environ['JAVA_HOME'] = os.path.join(os.getcwd(), 'share\\jdk8')
+os.environ['PATH'] = os.path.join(os.getcwd(), 'share\\mvn\\bin') + os.pathsep + os.environ['PATH']
+
 
 class MikroJ(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self._ij = imagej.init("sc.fiji:fiji:2.1.0", headless=False)
-
+        self.helper = ImageJHelper()
+        self.layout = QtWidgets.QHBoxLayout()
+        self.start_button = QtWidgets.QPushButton("Show")
+        self.start_button.clicked.connect(self.show_image)
+        self.setCentralWidget(self.start_button)
         self.init_ui()
 
+    def show_image(self):
+        array = xr.DataArray(np.zeros((3,1024,1024)), dims=list("cxy"), name="Nanana")
+        self.helper.show_xarray(array)
+        
+
     def init_ui(self):
-        self._ij.ui().showUI()
         self.setWindowTitle('MikroJ - fucking ImageJ')
         self.show()
 
