5dcf14b9e4440b70e4079fa6ba28617b9f9d9d84
jhnnsrs <jhnnsrs@gmail.com>
2022-10-11 12:13:06 +0200

    test

--- build-windows.py
+++ build-windows.py
@@ -1,15 +1,17 @@
 import PyInstaller.__main__
 
-APP_NAME = "KARL"
+APP_NAME = "MikroJ"
 
-PyInstaller.__main__.run([
-    'test.py',
-    '--clean',
-    '--windowed',
-    '--onefile',
-    f'--name={APP_NAME}',
-    '--noconfirm',
-    #'--add-data=share;share',
-    '--additional-hooks-dir=hooks',
-    '--icon=mikroj-logo.ico'
-])
+PyInstaller.__main__.run(
+    [
+        "test.py",
+        "--clean",
+        "--windowed",
+        "--onefile",
+        f"--name={APP_NAME}",
+        "--noconfirm",
+        "--add-data=share:share",
+        "--additional-hooks-dir=hooks",
+        "--icon=mikroj-logo.ico",
+    ]
+)
